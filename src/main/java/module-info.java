fb5cdf269f5f546aa5087d0dd06e6a67e3075563
Glavo <zjx001202@126.com>
2021-02-07 09:01:36 +0800

    export packages; provides TruffleLanguage with LuaLanguage

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,9 @@
 module org.glavo.lua {
     requires org.graalvm.truffle;
+
+    provides com.oracle.truffle.api.TruffleLanguage
+            with org.glavo.lua.LuaLanguage;
+
+    exports org.glavo.lua;
+    exports org.glavo.lua.data;
 }
