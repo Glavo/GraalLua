87aaffb627948eb49c3ed8718f5ab76d1f10d3ab
Glavo <zjx001202@126.com>
2021-02-08 21:39:19 +0800

    fix LuaTable#put

--- src/main/java/org/glavo/lua/runtime/LuaTable.java
+++ src/main/java/org/glavo/lua/runtime/LuaTable.java
@@ -9,6 +9,7 @@ public class LuaTable implements TruffleObject {
 
     private Object[] array;
     int arraySize = 0;
+    int arrayElementsCount = 0;
 
     private HashMap<Object, Object> table;
 
@@ -48,24 +49,33 @@ public class LuaTable implements TruffleObject {
         return table == null ? null : table.get(key);
     }
 
-    public final void set(Object key, Object newValue) {
+    public final Object put(Object key, Object newValue) {
         if (key == null || key == LuaNil.Nil) {
             throw new NullPointerException();
         }
-        setImpl(key, newValue == LuaNil.Nil ? null : newValue);
+        return putImpl(key, newValue == LuaNil.Nil ? null : newValue);
     }
 
-    final void setImpl(/* NotNull */ Object key, Object newValue) {
+    final Object putImpl(/* NotNull */ Object key, Object newValue) {
+        HashMap<Object, Object> table = this.table;
         if (key instanceof Integer) {
             final int kv = (Integer) key;
             if (kv > 0) {
                 final int as = this.arraySize;
+                final int idx = kv - 1;
                 if (kv <= as) {
-                    array[kv] = newValue;
-                    return;
-                } else if (kv == as + 1) {
+                    final Object[] array = this.array;
+                    Object oldValue = array[idx];
+                    if (newValue == null && oldValue != null) {
+                        --this.arrayElementsCount;
+                    } else if (newValue != null && oldValue == null) {
+                        ++this.arrayElementsCount;
+                    }
+                    array[idx] = newValue;
+                    return oldValue;
+                } else if (idx == as) {
                     if (newValue == null) {
-                        return;
+                        return table == null ? null : table.remove(key);
                     }
                     Object[] arr = this.array;
                     if (arr == null) {
@@ -77,20 +87,34 @@ public class LuaTable implements TruffleObject {
                         System.arraycopy(arr, 0, newArr, 0, as);
                         this.array = arr = newArr;
                     }
-                    arr[as] = newValue;
+                    arr[idx] = newValue;
                     ++this.arraySize;
-                    return;
+                    return table == null ? null : table.remove(key);
                 }
             }
         }
         if (newValue == null) {
-            table.remove(key);
+            return table == null ? null : table.remove(key);
         } else {
-            table.put(key, newValue);
+            if (table == null) {
+                this.table = (table = new HashMap<>());
+            }
+            return table.put(key, newValue);
         }
     }
 
     public final Map<Object, Object> asJavaMap() {
         throw new UnsupportedOperationException(); // TODO
+    }
+
+    @Override
+    public String toString() {
+        //TODO
+        return "LuaTable{" +
+                "array=" + Arrays.toString(array) +
+                ", arraySize=" + arraySize +
+                ", arrayElementsCount=" + arrayElementsCount +
+                ", table=" + table +
+                '}';
     }
 }
