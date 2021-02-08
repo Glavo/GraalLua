3352bdd1650dc44f1e10b80e8e575f72abbd49a7
Glavo <zjx001202@126.com>
2021-02-08 21:53:59 +0800

    Configure test environment

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,17 +5,29 @@ plugins {
 group = "org.glavo"
 version = "0.1.0"
 
-tasks.compileJava {
-    modularity.inferModulePath.set(true)
-    options.release.set(11)
-}
+val graalvmVersion: String by project
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    val graalVersion = "21.0.0"
-    implementation("org.graalvm.truffle:truffle-api:$graalVersion")
-    annotationProcessor("org.graalvm.truffle:truffle-dsl-processor:$graalVersion")
+    implementation("org.graalvm.truffle:truffle-api:$graalvmVersion")
+    annotationProcessor("org.graalvm.truffle:truffle-dsl-processor:$graalvmVersion")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
+}
+
+tasks.compileJava {
+    modularity.inferModulePath.set(true)
+    options.release.set(11)
+}
+
+tasks.compileTestJava {
+    options.release.set(11)
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging.showStandardStreams = true
 }
